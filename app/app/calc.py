c345126384e7132ae028706ca6dd8a3cef987a3d
viggo <pythonmayor@gmail.com>
2024-06-02 01:37:21 +0200

    Added postgres adapter

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+'''
+calculator functions
+'''
+
+def add(x, y):
+    '''
+    Add two numbers together and return result
+    '''
+    return x + y
+
+def subtract(x, y):
+    '''
+    Subtract x from y and return result
+    '''
+    return y - x
