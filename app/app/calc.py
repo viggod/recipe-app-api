d2d5ed1b5b9293271e27a851201061b4c283f8fd
viggo <pythonmayor@gmail.com>
2024-06-05 06:49:57 +0200

    Configured docker-compose and checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,15 +1,17 @@
-'''
+"""
 calculator functions
-'''
+"""
+
 
 def add(x, y):
-    '''
+    """
     Add two numbers together and return result
-    '''
+    """
     return x + y
 
+
 def subtract(x, y):
-    '''
+    """
     Subtract x from y and return result
-    '''
+    """
     return y - x
