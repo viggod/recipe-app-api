f3672110acbaf9bb4658b8fe24a2a19b953f3006
viggo <pythonmayor@gmail.com>
2024-06-07 23:05:59 +0200

    test failed, check code

--- app/user/views.py
+++ app/user/views.py
@@ -20,3 +20,14 @@ class CreateTokenView(ObtainAuthToken):
     """Create a new auth token for user"""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated, ]
+    authentication_classes = [authentication.TokenAuthentication, ]
+
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return self.request.user
