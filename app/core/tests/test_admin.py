fad55099d353859da7a23dfb8920f69b55ae0531
viggo <pythonmayor@gmail.com>
2024-06-07 22:33:41 +0200

    added token api

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,5 +1,4 @@
 """Test for the Django admin modification"""
-
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -11,14 +10,14 @@ class AdminSiteTests(TestCase):
         """Setup function for the test. Create user and a client"""
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email = 'admin@example.com',
-            password = 'testpass123',
+            email='admin@example.com',
+            password='testpass123',
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email = 'user@example.com',
-            password = 'testpass123',
-            name = 'Test user full name',
+            email='user@example.com',
+            password='testpass123',
+            name='Test user full name',
         )
 
     def test_users_list(self):
@@ -28,3 +27,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
---
+++ app/user/views.py
@@ -0,0 +1,22 @@
+"""Views for the user API"""
+
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializer import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = (authentication.TokenAuthentication,)
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
