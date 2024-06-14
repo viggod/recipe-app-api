064578f3d85948b9f1961680ad41364939d40b88
viggo <pythonmayor@gmail.com>
2024-06-14 21:14:44 +0200

    Implemented image API to recipe

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,4 +1,5 @@
 """Tests for models"""
+from unittest.mock import patch
 from decimal import Decimal
 
 from django.test import TestCase
@@ -81,3 +82,14 @@ class ModelsTests(TestCase):
         ingredient = models.Ingredient.objects.create(user=user, name='Salt')
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test generating image path that image is saved in the correct
+        location"""
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        exp_path = f'uploads/recipe/{uuid}.jpg'
+        self.assertEqual(file_path, exp_path)
